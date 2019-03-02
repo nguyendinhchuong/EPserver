d2fd4898f054f6692aadbdf745e4940ef430565e
NDChuong <nguyendinhchuong2607@gmail.com>
2019-03-02 10:15:33 +0700

    add db.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var app = express();
+app.listen(3000,function(){
+    console.log('Node server running @ http://localhost:3000')
+});
---
+++ db.js
@@ -0,0 +1,50 @@
+var mysql = require('mysql');
+
+exports.load = sql => {
+    return new Promise((resolve, reject) => {
+        var cn = mysql.createConnection({
+            host: 'localhost',
+            port: 3000,
+            user: 'root',
+            database: 'educationprogram'
+        });
+
+        cn.connect((err) => {
+            if (err) throw err;
+            console.log("Connected!!!")
+        });
+
+        cn.query(sql, function (error, rows, fields) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(rows);
+            }
+
+            cn.end();
+        });
+    });
+}
+
+exports.save = sql => {
+    return new Promise((resolve, reject) => {
+        var cn = mysql.createConnection({
+            host: 'localhost',
+            port: 3000,
+            user: 'root',
+            database: 'educationprogram'
+        });
+
+        cn.connect();
+
+        cn.query(sql, function (error, value) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(value);
+            }
+
+            cn.end();
+        });
+    });
+}
