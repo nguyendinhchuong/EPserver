478dd8a2c4f795a358da0187f358f60650d53909
Minh Nhat <mocking.key118@gmail.com>
2019-03-03 17:34:11 +0700

    change getOutcomeStandard's query

--- service/outcomeStandardService.js
+++ service/outcomeStandardService.js
@@ -1,14 +1,14 @@
-const db = require('../db');
-const nameTable = require('../config/nameTable');
+const db = require("../db");
+const nameTable = require("../config/nameTable");
 
-exports.getOutcomeStandards = () =>{
-    const sql = `
-    select f.Id as 'IdFaculty', p.Id as 'IdProgram', o.Id as 'IdOutcome' , o.NameOutcome 'NameOutcomeStandard',
-    f.NameFaculty 'NameFaculty' ,  p.NameProgram 'NameProgram', pf.DateCareated, pf.DateEdited   
+exports.getOutcomeStandards = () => {
+  const sql = `
+    select f.Id as 'IdFaculty', p.Id as 'IdProgram', o.Id as 'IdOutcome',
+    f.NameFaculty 'NameFaculty',  p.NameProgram 'NameProgram', pf.DateCareated, pf.DateEdited   
     from ${nameTable.ProgramFaculty} as pf 
     left join ${nameTable.Faculty}  as f on pf.IdFaculty = f.Id
     left join ${nameTable.Program} as p on pf.IdProgram = p.Id
     right join ${nameTable.OutcomeStandard} as o on o.Id = pf.Id
-    `;  
-    return db.load(sql);
-}
+    `;
+  return db.load(sql);
+};
