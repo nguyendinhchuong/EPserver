f69d4c9347a97d1b1b1ce4d18634f6e534813ea2
docaotri123 <docaotri211997@gmail.com>
2019-03-03 11:53:13 +0700

    [1512602-merge ]

--- db.js
+++ db.js
@@ -4,19 +4,11 @@ const dbConfig = require('./config/dbConfig');
 exports.load = sql => {
     return new Promise((resolve, reject) => {
         const cn = mysql.createConnection({
-<<<<<<< HEAD
             host: dbConfig.host,
             port: dbConfig.port,
             user: dbConfig.user,
             password: dbConfig.password,
             database: dbConfig.database    
-=======
-            host: 'localhost',
-            port: 3306,
-            user: 'root',
-            password: '',
-            database: 'cdio'            
->>>>>>> 80fe81301e9e94f08cfb2799ef98af3a840f2d6b
         });
         cn.connect();
 
@@ -36,22 +28,11 @@ exports.load = sql => {
 exports.save = sql => {
     return new Promise((resolve, reject) => {
         const cn = mysql.createConnection({
-<<<<<<< HEAD
             host: dbConfig.host,
             port: dbConfig.port,
             user: dbConfig.user,
             password: dbConfig.password,
             database: dbConfig.database  
-=======
-            host: 'localhost',
-            port: 3306,
-            user: 'root',
-            password: '',
-            database: 'cdio'
-<<<<<<< HEAD
->>>>>>> 80fe81301e9e94f08cfb2799ef98af3a840f2d6b
-=======
->>>>>>> 80fe81301e9e94f08cfb2799ef98af3a840f2d6b
         });
 
         cn.connect();
