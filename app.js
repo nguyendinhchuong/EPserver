9f5f50981f7ccf42e36bc5696412d8e9f08a418f
docaotri123 <docaotri211997@gmail.com>
2019-03-03 16:05:02 +0700

    [1512602] add columns outcome

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const routesOutcomes = require('./routes/outcomeStandardRoute');
 const routesPrograms = require('./routes/programRoute');
 const routesFaculties = require('./routes/facultyRoute');
 
-routesOutcomes(app);
+
 routesPrograms(app);
 routesFaculties(app);
+routesOutcomes(app);
 
-app.listen(3000,() => {
-    console.log('Node server running @ http://localhost:3000')
+app.listen(3001,() => {
+    console.log('Node server running @ http://localhost:3001')
 });
