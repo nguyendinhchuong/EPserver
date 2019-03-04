99966a906db3dc91b0ae6c6d1175666df6fde248
Minh Nhat <mocking.key118@gmail.com>
2019-03-04 13:07:58 +0700

    change query of getDetailProgramFaculty

--- service/programFacultyService.js
+++ service/programFacultyService.js
@@ -3,8 +3,8 @@ const nameTable = require('../config/nameTable');
 
 exports.getDetailProgramFaculty = (idOutcome) =>{
     const sql = `
-    select f.Id as 'IdFaculty', p.Id as 'IdProgram', o.Id as 'IdOutcome' , o.NameOutcome 'NameOutcomeStandard',
-    f.NameFaculty 'NameFaculty' ,  p.NameProgram 'NameProgram', pf.DateCareated, pf.DateEdited 
+    select f.Id as 'IdFaculty', p.Id as 'IdProgram', o.Id as 'IdOutcome',
+    f.NameFaculty 'NameFaculty',  p.NameProgram 'NameProgram', pf.DateCareated, pf.DateEdited 
     from ${nameTable.ProgramFaculty} as pf 
     left join ${nameTable.Faculty}  as f on pf.IdFaculty = f.Id
     left join ${nameTable.Program} as p on pf.IdProgram = p.Id
