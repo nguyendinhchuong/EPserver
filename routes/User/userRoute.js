74cf8813e676ace3066d1e7712ac377598befd2a
NDChuong <nguyendinhchuong2607@gmail.com>
2019-05-02 00:38:22 +0700

    remove auth in api get list user

--- routes/User/userRoute.js
+++ routes/User/userRoute.js
@@ -1,7 +1,8 @@
 const user = require('../../controllers/User/userController');
 const auth = require('../../controllers/User/authController');
 module.exports = (app) => {
-    app.route('/user/getlist').get(auth.isAuthenticated, user.getList);
+    // app.route('/user/getlist').get(auth.isAuthenticated, user.getList);
+    app.route('/user/getlist').get(user.getList);
     app.route('/user/getinfo').get(auth.isAuthenticated, user.getInfo);
 
     app.route('/user/register').post(auth.isAuthenticated, user.register);
